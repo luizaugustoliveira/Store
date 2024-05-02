e8d164019f7a690fa90f848d16a6db7afe1a5e78
Luiz Augusto Oliveira <luiz.augusto.farias@ccc.ufcg.edu.br>
2024-05-02 19:01:35 -0300

    add update product list

--- vtexstore/src/components/products-list.tsx
+++ vtexstore/src/components/products-list.tsx
@@ -4,6 +4,18 @@ import { useProducts } from "@/hooks/useProducts"
 import { ProductCard } from "./product-card";
 import { styled } from "styled-components";
 
+/*
+    useProducts: um hook para buscar produtos.
+    ProductCard: um componente para exibir informações individuais do produto.
+    styled: função do styled-components para estilização CSS em JavaScript.
+
+    Usa o hook useProducts para obter os dados dos produtos, que inclui uma lista de produtos disponíveis.
+    Renderiza o ListContainer, e, dentro dele, mapeia cada produto para um componente ProductCard, passando as propriedades necessárias (id, título, preço e imagem).
+    Cada ProductCard é renderizado com uma chave única (key)
+*/
+
+
+
 const ListContainer = styled.div`
     display: grid;
     grid-template-columns: repeat(4, 1fr);
